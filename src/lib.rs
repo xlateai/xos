8429398b27c5233028967125492124ffdb68882f
verbiiyo <dyllanmccreary@gmail.com>
2025-03-12 23:57:35 -0700

    start wasm support

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,19 @@ pub mod audio;
 pub mod viewport;
 
 pub mod waveform;
+
+// Add WebAssembly bindings
+#[cfg(target_arch = "wasm32")]
+pub mod wasm {
+    use wasm_bindgen::prelude::*;
+    use super::viewport;
+
+    #[wasm_bindgen]
+    pub fn open_viewport() {
+        viewport::open_viewport();
+    }
+}
+
+// Only include wasm-bindgen when targeting WebAssembly
+#[cfg(target_arch = "wasm32")]
+extern crate wasm_bindgen;
