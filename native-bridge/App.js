1e6576a11d8c8c516751fe7c44e1d2788c962979
verbii <dyllanmccreary@gmail.com>
2025-03-23 13:28:10 +0000

    just a blank black screen

--- native-bridge/App.js
+++ native-bridge/App.js
@@ -1,20 +1,41 @@
-import { WebView } from 'react-native-webview';
-import { Asset } from 'expo-asset';
-import { Platform } from 'react-native';
-
-const assetUri = Asset.fromModule(require('./assets/web/index.html')).uri;
+import { Platform } from "react-native";
+import { WebView } from "react-native-webview";
+import * as Asset from "expo-asset";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
 
 export default function App() {
+  const [htmlUri, setHtmlUri] = useState(null);
+
+  useEffect(() => {
+    if (Platform.OS !== "web") {
+      const asset = Asset.Asset.fromModule(require("./assets/web/index.html"));
+      asset.downloadAsync().then(() => {
+        setHtmlUri(asset.localUri);
+      });
+    }
+  }, []);
+
+  if (Platform.OS === "web") {
+    return (
+      <iframe
+        src="/assets/web/index.html"
+        style={{ flex: 1, width: "100%", height: "100%", border: "none" }}
+      />
+    );
+  }
+
   return (
-    <WebView
-      originWhitelist={['*']}
-      source={
-        Platform.OS === 'web'
-          ? { uri: '/assets/web/index.html' } // optional for Expo web
-          : { uri: assetUri }
-      }
-      javaScriptEnabled
-      allowsInlineMediaPlayback
-    />
+    <View style={{ flex: 1 }}>
+      {htmlUri && (
+        <WebView
+          originWhitelist={["*"]}
+          source={{ uri: htmlUri }}
+          allowFileAccess
+          javaScriptEnabled
+          allowsInlineMediaPlayback
+        />
+      )}
+    </View>
   );
 }
