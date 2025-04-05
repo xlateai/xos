71022b32026a22b1fe8c38742e72635de81baac6
verbiiyo <dyllanmccreary@gmail.com>
2025-04-05 13:05:41 +0900

    we need to optimize this

--- python/run.py
+++ python/run.py
@@ -1,13 +1,15 @@
 import xospy
 
 # run a rust compiled game with the game name here
-xospy.run_game("tracers", web=False, react_native=False)
+# xospy.run_game("tracers", web=False, react_native=False)
 
-# class PyApp(xospy.ApplicationBase):
-#     def setup(self, width: int, height: int):
-#         pass
+class PyApp(xospy.ApplicationBase):
+    def setup(self, width: int, height: int):
+        self.counter = 0
 
-#     def tick(self, width: int, height: int):
-#         return [0, 100, 100, 255] * (width * height)  # RGBA black pixels
+    def tick(self, width: int, height: int):
+        self.counter += 1
+        print("tick", width, height, self.counter)
+        return [0, 100, 100, 255] * (width * height)
 
-# xospy.run_py_game(PyApp(), web=False, react_native=False)
+xospy.run_py_game(PyApp(), web=False, react_native=False)
