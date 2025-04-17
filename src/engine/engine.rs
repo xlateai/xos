278e345c42af388cccba7dd772696f0c9768ad47
verbiiyo <dyllanmccreary@gmail.com>
2025-04-18 06:16:43 +0900

    introduce mouse.style.{styles}

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -1,30 +1,64 @@
 #[derive(Debug, Clone)]
-pub struct EngineState {
-    pub frame: FrameState,
-    pub mouse: MouseState,
-}
-
-#[derive(Debug, Clone)]
 pub struct FrameState {
     pub width: u32,
     pub height: u32,
     pub buffer: Vec<u8>,
 }
 
-#[derive(Debug, Clone)]
-pub struct MouseState {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum CursorStyle {
+    Default,
+    Text,
+    ResizeHorizontal,
+    ResizeVertical,
+    ResizeDiagonalNE,
+    ResizeDiagonalNW,
+    Hand,
+    Crosshair,
+}
+
+pub struct CursorStyleSetter<'a> {
+    style: &'a mut CursorStyle,
+}
+
+impl<'a> CursorStyleSetter<'a> {
+    fn set(&mut self, val: CursorStyle) {
+        *self.style = val;
+    }
+}
+
+macro_rules! impl_cursor_style_setters {
+    ($($variant:ident => $method:ident),* $(,)?) => {
+        impl<'a> CursorStyleSetter<'a> {
+            $(
+                #[inline(always)]
+                pub fn $method(&mut self) {
+                    self.set(CursorStyle::$variant);
+                }
+            )*
+        }
+    };
+}
+
+impl_cursor_style_setters! {
+    Default => default,
+    Text => text,
+    ResizeHorizontal => resize_horizontal,
+    ResizeVertical => resize_vertical,
+    ResizeDiagonalNE => resize_diagonal_ne,
+    ResizeDiagonalNW => resize_diagonal_nw,
+    Hand => hand,
+    Crosshair => crosshair,
+}
+
+pub struct MouseState<'a> {
     pub x: f32,
     pub y: f32,
     pub is_down: bool,
+    pub style: CursorStyleSetter<'a>,
 }
 
-pub trait Application {
-    fn setup(&mut self, state: &mut EngineState) -> Result<(), String>;
-    fn tick(&mut self, state: &mut EngineState);
-
-    fn on_mouse_down(&mut self, _state: &mut EngineState) {}
-    fn on_mouse_up(&mut self, _state: &mut EngineState) {}
-    fn on_mouse_move(&mut self, _state: &mut EngineState) {}
-    fn on_scroll(&mut self, _state: &mut EngineState, _delta_x: f32, _delta_y: f32) {}
-    fn on_key_char(&mut self, _state: &mut EngineState, _ch: char) {}
+pub struct EngineState<'a> {
+    pub frame: FrameState,
+    pub mouse: MouseState<'a>,
 }
