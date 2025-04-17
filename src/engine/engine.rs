705bb9fcace6691e47a6e70e3d37aca11f206dad
verbiiyo <dyllanmccreary@gmail.com>
2025-04-18 06:19:24 +0900

    woops didn't mean to delete this

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -62,3 +62,14 @@ pub struct EngineState<'a> {
     pub frame: FrameState,
     pub mouse: MouseState<'a>,
 }
+
+pub trait Application {
+    fn setup(&mut self, state: &mut EngineState) -> Result<(), String>;
+    fn tick(&mut self, state: &mut EngineState);
+
+    fn on_mouse_down(&mut self, _state: &mut EngineState) {}
+    fn on_mouse_up(&mut self, _state: &mut EngineState) {}
+    fn on_mouse_move(&mut self, _state: &mut EngineState) {}
+    fn on_scroll(&mut self, _state: &mut EngineState, _delta_x: f32, _delta_y: f32) {}
+    fn on_key_char(&mut self, _state: &mut EngineState, _ch: char) {}
+}
