9990d2de1773798f35216fb8386b8e1126b92cc5
verbiiyo <dyllanmccreary@gmail.com>
2025-10-18 11:13:35 -0400

    subapp testing

--- src/apps/synthesis/synthesis.rs
+++ src/apps/synthesis/synthesis.rs
@@ -1,33 +1,28 @@
 use crate::engine::{Application, EngineState};
+use crate::apps::waveform::Waveform;
 
 const BACKGROUND_COLOR: (u8, u8, u8) = (32, 32, 32); // Dark gray
 
-pub struct SynthesisApp;
+pub struct SynthesisApp {
+    waveform: Waveform,
+}
 
 impl SynthesisApp {
     pub fn new() -> Self {
-        Self
+        Self {
+            waveform: Waveform::new(),
+        }
     }
 }
 
 impl Application for SynthesisApp {
     fn setup(&mut self, _state: &mut EngineState) -> Result<(), String> {
-        Ok(())
+    self.waveform.setup(_state)?;
+    Ok(())
     }
 
     fn tick(&mut self, state: &mut EngineState) {
-        // Change this line:
-        // From: let mut buffer = &state.frame.buffer;
-        // To: let buffer = &mut state.frame.buffer;
-        let buffer = &mut state.frame.buffer;
-        let len = buffer.len();
-
-        for i in (0..len).step_by(4) {
-            buffer[i + 0] = BACKGROUND_COLOR.0;
-            buffer[i + 1] = BACKGROUND_COLOR.1;
-            buffer[i + 2] = BACKGROUND_COLOR.2;
-            buffer[i + 3] = 0xff;
-        }
+    self.waveform.tick(state);
     }
 
     fn on_mouse_down(&mut self, _state: &mut EngineState) {
