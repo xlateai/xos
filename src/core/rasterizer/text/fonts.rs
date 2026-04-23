84c3027a04ef4ec74d7b52b16e0908e27c15e7fe
verbiiyo <dyllanmccreary@gmail.com>
2026-04-23 17:32:34 +0900

    lets gooo best font ever

--- src/core/rasterizer/text/fonts.rs
+++ src/core/rasterizer/text/fonts.rs
@@ -10,3 +10,8 @@ pub fn noto_sans_jp() -> Font {
     let font_bytes = include_bytes!("../../assets/NotoSansJP-Regular.ttf") as &[u8];
     Font::from_bytes(font_bytes, FontSettings::default()).unwrap()
 }
+
+pub fn dot_gothic_16() -> Font {
+    let font_bytes = include_bytes!("../../assets/DotGothic16-Regular.ttf") as &[u8];
+    Font::from_bytes(font_bytes, FontSettings::default()).unwrap()
+}
