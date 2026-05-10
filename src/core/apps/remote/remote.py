5e6884f8f620c9729d29eda02c1b8cb9b0a9bc34
verbiiyo <dyllanmccreary@gmail.com>
2026-05-11 03:15:59 +0900

    hello world text for the remote app

---
+++ src/core/apps/remote/remote.py
@@ -0,0 +1,29 @@
+import xos
+
+
+class RemoteApp(xos.Application):
+    headless: bool = False
+
+    def __init__(self):
+        super().__init__()
+
+        self.text = xos.ui.text(
+            "Hello, World!",
+            x1=0.0,
+            y1=0.0,
+            x2=1.0,
+            y2=1.0,
+            alignment=(0.5, 0.5),
+            color=xos.color.WHITE,
+            size=36.0,
+        )
+
+        # self.mesh = xos.mesh.connect(id=constants.MESH_CHANNEL, mode=constants.MODE, udp=constants.USE_UDP)
+
+    def tick(self):
+        self.frame.clear(xos.color.BLACK)
+        self.text.tick(self)
+        self.text.render(self)
+
+if __name__ == "__main__":
+    RemoteApp().run()
