65b0c0f4dc388c4c8c2ce8e925dce101ce701e7c
verbiiyo <dyllanmccreary@gmail.com>
2026-05-10 23:43:58 +0900

    connection between viewer and non-viewer

--- src/core/apps/remote/viewer.py
+++ src/core/apps/remote/viewer.py
@@ -10,34 +10,41 @@ import utils
 
 
 class RemoteViewerApp(xos.Application):
+    headless: bool = True
 
     def __init__(self):
+        super().__init__()
+
         self.mesh = xos.mesh.connect(id=utils.MESH_CHANNEL, mode=utils.MODE)
-        self.status = xos.ui.text(
-            "Connecting...",
-            x1=0.0,
-            y1=0.0,
-            x2=1.0,
-            y2=0.1,
-            color=xos.color.CYAN,
-        )
-        self.video = xos.ui.video(
-            x1=0.0,
-            y1=0.0,
-            x2=1.0,
-            y2=1.0,
-        )
+        # self.status = xos.ui.text(
+        #     "Connecting...",
+        #     x1=0.0,
+        #     y1=0.0,
+        #     x2=1.0,
+        #     y2=0.1,
+        #     size=36.0,
+        #     color=xos.color.CYAN,
+        # )
+        # self.video = xos.ui.video(
+        #     x1=0.0,
+        #     y1=0.0,
+        #     x2=1.0,
+        #     y2=1.0,
+        # )
 
     def tick(self):
-        self.status.tick(self)
-        self.status.render(self)
+        # self.status.tick(self)
+        # self.status.render(self)
 
-        self.video.tick(self)
-        self.video.render(self)
+        # self.video.tick(self)
+        # self.video.render(self)
 
-        packet = self.mesh.receive(id="frame", wait=False, latest_only=False)
+        packet = self.mesh.receive(id="frame", wait=False, latest_only=True)
         if packet:
-            self.video.frame = packet
+            print(packet.frame)
+            # self.video.frame = packet
+
+        # print(self.t)
 
 
 
